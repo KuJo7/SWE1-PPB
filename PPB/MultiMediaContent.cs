d589f5b31ecaf0c3e8c00369ea9bb917d493d754
Joel Kudiyirickal <if19b226@technikum-wien.at>
2021-02-04 01:34:22 +0100

    created classes

---
+++ PPB/MultiMediaContent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPB
+{
+    public class MultiMediaContent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        string Filetype { get; set; }
+        int Filesize { get; set; }
+        string Title { get; set; }
+        string Artist { get; set; }
+        string Album { get; set; }
+        int Rating { get; set; }
+        string Genre { get; set; }
+        int Length { get; set; }
+        string Url { get; set; }
+    }
+}
+
---
+++ PPB/Scoreboard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace PPB
+{
+    public class Scoreboard
+    {
+
+        public List<User> GetUserswithMostWins()
+        {
+
+            var users = DBHandler.GetallUsers();
+            var sortedUsers = users.OrderByDescending(user => user.Wins).ToList();
+             
+            return sortedUsers;
+        }
+
+        public List<User> GetUserswithMostLosses()
+        {
+
+            var users = DBHandler.GetallUsers();
+            var sortedUsers = users.OrderByDescending(user => user.Losses).ToList();
+
+            return sortedUsers;
+        }
+
+        public List<User> GetUserswithWinsLossesRatio()
+        {
+
+            var users = DBHandler.GetallUsers();
+            var sortedUsers = users.OrderByDescending(user => user.Wins/user.Losses).ToList();
+
+            return sortedUsers;
+        }
+
+        public List<User> GetUserswithHighestElo()
+        {
+
+            var users = DBHandler.GetallUsers();
+            var sortedUsers = users.OrderByDescending(user => user.Elo).ToList();
+
+            return sortedUsers;
+        }
+    }
+}
