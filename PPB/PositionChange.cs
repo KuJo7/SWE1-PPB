e124c4025ac95240adb5a668a8ce7c40385c5327
Joel Kudiyirickal <if19b226@technikum-wien.at>
2021-02-18 19:00:26 +0100

    Create PositionChange.cs

---
+++ PPB/PositionChange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPB
+{
+    public class PositionChange
+    {
+        private int _fromPosition;
+        private int _toPosition;
+        public int FromPosition
+        {
+            get => _fromPosition - 1;
+            set => _fromPosition = value;
+        }
+        public int ToPosition
+        {
+            get => _toPosition - 1;
+            set => _toPosition = value;
+        }
+    }
+}
