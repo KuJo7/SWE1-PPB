90bafbacc2eb207297ca4ac978d5a9c54d16209d
Joel Kudiyirickal <if19b226@technikum-wien.at>
2021-02-18 19:00:06 +0100

    Create ActionString.cs

---
+++ PPB/ActionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPB
+{
+    public class ActionString
+    {
+        public string Actions { get; set; }
+    }
+}
