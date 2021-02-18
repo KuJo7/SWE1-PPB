e5ab3746727b462581bb836442d5d3f51cedc7bb
Joel Kudiyirickal <if19b226@technikum-wien.at>
2021-02-18 18:59:18 +0100

    Update User.cs

--- PPB/User.cs
+++ PPB/User.cs
@@ -9,26 +9,28 @@ namespace PPB
         public Guid UserId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string Name { get; set; } = "";
-        public string Bio { get; set; } = "";
-        public string Image { get; set; } = "";
-        public int Elo { get; set; } = 100;
-        public string Rank => Elo switch
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+        public int TournamentPoints { get; set; } = 100;
+        public string Rank => TournamentPoints switch
         {
             < 101 => "Noob",
-            > 100 and <= 120 => "Bronze",
-            > 120 and <= 140 => "Silver",
-            > 140 and <= 160 => "Gold",
-            > 160 and <= 180 => "Diamond",
-            > 180 and <= 200 => "Platinum",
-            > 200 => "Challenger"
+            > 100 and <= 105 => "Bronze",
+            > 105 and <= 110 => "Silver",
+            > 110 and <= 115 => "Gold",
+            > 115 and <= 120 => "Diamond",
+            > 120 and <= 125 => "Platinum",
+            > 125 => "Challenger"
         };
-        public List<MultiMediaContent> Library { get; set; }
-        public List<Actions> Actions { get; set; }
+        public List<MultiMediaContent> Library { get; set; } = new List<MultiMediaContent>();
+        public List<Actions> Actions { get; set; } = new List<Actions>();
         public int Wins { get; set; } = 0;
         public int Losses { get; set; } = 0;
-        public bool IsAdmin { get; set; } = false;
+        public int Draws { get; set; } = 0;
+        public bool IsBlocked { get; set; } = false;
         public int BattlePoints { get; set; } = 0;
+        public int GamesPlayed { get; set; } = 0;
     }
 
 
