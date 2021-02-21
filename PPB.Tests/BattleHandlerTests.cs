0c36984b983e7b2a962def4111f318268f9dd46a
Joel Kudiyirickal <if19b226@technikum-wien.at>
2021-02-21 18:30:50 +0100

    Create DBHandlerTests.cs

--- PPB.Tests/BattleHandlerTests.cs
+++ PPB.Tests/BattleHandlerTests.cs
@@ -1,18 +1,85 @@
 using NUnit.Framework;
+using System.Net;
+using System.Collections.Generic;
 
 namespace PPB.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+      
+
+        [Test]
+        public void TestBattleWinLoss()
         {
+            // Arrange
+            User player1 = new User() { Actions = new List<Actions>() { Actions.R,Actions.R, Actions.R, Actions.R, Actions.R } };
+            User player2 = new User() { Actions = new List<Actions>() { Actions.S, Actions.S, Actions.S, Actions.S, Actions.S } };
+
+            // Act
+            var result = BattleHandler.Battle(player1, player2);
+
+            // Assert
+            Assert.AreEqual(player1, result.winner);
+            Assert.AreEqual(player2, result.loser);
         }
 
         [Test]
-        public void Test1()
+        public void TestBattleDraw()
         {
-            Assert.Pass();
+            // Arrange
+            User player1 = new User() { Actions = new List<Actions>() { Actions.R, Actions.R, Actions.R, Actions.R, Actions.R } };
+            User player2 = new User() { Actions = new List<Actions>() { Actions.R, Actions.R, Actions.R, Actions.R, Actions.R } };
+
+            // Act
+            var result = BattleHandler.Battle(player1, player2);
+
+            // Assert
+            Assert.AreEqual(null, result.winner);
+            Assert.AreEqual(null, result.loser);
+        }
+
+        [Test]
+        public void TestActionBattleWin()
+        {
+            // Arrange
+            User player1 = new User() { Actions = new List<Actions>() { Actions.R, Actions.R, Actions.R, Actions.R, Actions.R } };
+            User player2 = new User() { Actions = new List<Actions>() { Actions.S, Actions.S, Actions.S, Actions.S, Actions.S } };
+            List<string> log = new List<string>();
+            // Act
+            var result = BattleHandler.ActionBattle(player1, player2, log);
+
+            // Assert
+            Assert.AreEqual(player1, result);
+        }        
+        
+        [Test]
+        public void TestActionBattleLoss()
+        {
+            // Arrange
+            User player1 = new User() { Actions = new List<Actions>() { Actions.S, Actions.S, Actions.S, Actions.S, Actions.S } };
+            User player2 = new User() { Actions = new List<Actions>() { Actions.R, Actions.R, Actions.R, Actions.R, Actions.R } };
+            List<string> log = new List<string>();
+
+            // Act
+            var result = BattleHandler.ActionBattle(player1, player2, log);
+
+            // Assert
+            Assert.AreEqual(player2, result);
+        }
+
+        [Test]
+        public void TestActionBattleDraw()
+        {
+            // Arrange
+            User player1 = new User() { Actions = new List<Actions>() { Actions.R, Actions.R, Actions.R, Actions.R, Actions.R } };
+            User player2 = new User() { Actions = new List<Actions>() { Actions.R, Actions.R, Actions.R, Actions.R, Actions.R } };
+            List<string> log = new List<string>();
+
+            // Act
+            var result = BattleHandler.ActionBattle(player1, player2, log);
+
+            // Assert
+            Assert.AreEqual(null, result);
         }
     }
 }
---
+++ PPB.Tests/DBHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Net;
+
+
+namespace PPB.Tests
+{
+    class DBHandlerTests
+    {
+
+        [Test]
+        public void TestGetAllUsers()
+        {
+            // Act
+            var result = DBHandler.GetAllUsers();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void TestGetUser()
+        {
+
+            // Arrange
+            string username = "kienboec"; 
+            // Act
+            var result = DBHandler.GetUser(username);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.AreEqual(username, result.Username);
+        }
+    }
+}
